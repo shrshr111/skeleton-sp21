0010002af15be5751ba1ae165fa1abd9c6f7b161
Zifan <zfye1012@gmail.com>
2022-06-09 21:49:16 -0500

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,23 +5,26 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n  == 1) {
+            return 0;
+        } else if (n % 2 == 0) {
+            return n /2 ;
         } else {
-            return n * 2;
+            return 3 * n + 1;
         }
     }
 
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");
-        while (n != 1) {
-            n = nextNumber(n);
+
+        while (true) {
             System.out.print(n + " ");
+            n = nextNumber(n);
+            if(n == 1){
+                break;
+            }
         }
-        System.out.println();
+
     }
 }
 
